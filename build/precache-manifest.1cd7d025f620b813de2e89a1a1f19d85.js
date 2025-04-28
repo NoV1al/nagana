0c2ec4eaa9e93d2b8c5ec3a0f2fdaca180e10c96
NoV1al <vincexxd.allen@gmail.com>
2025-04-28 16:12:43 +0800

    Add files via upload

---
+++ build/precache-manifest.1cd7d025f620b813de2e89a1a1f19d85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8936db2030998c2aadfb0e7cd1b8db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a3295e726ff6939f224",
+    "url": "/static/css/main.6c160fbd.chunk.css"
+  },
+  {
+    "revision": "e351faa319568d647ac7",
+    "url": "/static/js/2.11cfe5f5.chunk.js"
+  },
+  {
+    "revision": "5a3295e726ff6939f224",
+    "url": "/static/js/main.6ea94075.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f95b022c7fffca0eb51708294c776765",
+    "url": "/static/media/about.f95b022c.jpg"
+  },
+  {
+    "revision": "8dffa736673b83d4ca4b78da67bb1e78",
+    "url": "/static/media/home.8dffa736.png"
+  },
+  {
+    "revision": "ee92d5a64ef036c896779c814f5ae336",
+    "url": "/static/media/wizard.ee92d5a6.webp"
+  }
+]);
